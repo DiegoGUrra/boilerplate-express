e57474541f077bb476c8a2da33fd63bdc0588030
DiegoGUrra <clicknamk@gmail.com>
2022-09-22 04:02:43 -0300

    added env uppercase

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 let express = require('express');
 let app = express();
 //console.log("Hello World");
@@ -5,7 +6,12 @@ app.get("/",function (req, res) {
         res.sendFile(__dirname+"/views/index.html");
     });
 app.use("/public",express.static(__dirname+"/public"));
-app.get("/json",(req,res)=> res.json({"message": "Hello json"}));
+app.get("/json",(req,res)=>{ 
+    let message = {"message": "Hello json"};
+    if (process.env.MESSAGE_STYLE==="uppercase")
+        message.message.toUpperCase();
+    res.json(message);
+});
 
 
 
